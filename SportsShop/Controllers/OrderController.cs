01f3408a65be55c2314c76784391941b25efbf21
maqib <muhammadaqib1831@outlook.com>
2018-07-31 12:24:14 +0500

    Admin panel

--- SportsShop/Controllers/OrderController.cs
+++ SportsShop/Controllers/OrderController.cs
@@ -16,6 +16,18 @@ namespace SportsShop.Controllers
             repository = repoService;
             cart = cartService;
         }
+        public ViewResult List() => View(repository.Orders.Where(o=>!o.Shipped));
+
+        public IActionResult MarkShipped(int orderID)
+        {
+            Order order = repository.Orders.FirstOrDefault(o=>o.OrderID==orderID);
+            if (order != null)
+            {
+                order.Shipped = true;
+                repository.SaveOrder(order);
+            }
+            return RedirectToAction(nameof(List));
+        }
         public ViewResult Checkout() => View(new Order());
         [HttpPost]
         public IActionResult Checkout(Order order)
